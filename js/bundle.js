64d78efa392fe48d1f7c6e434b6e6dd6d9b4bb5e
Max Chernyshov <chernyshovmaksim56@gmail.com>
2019-05-13 15:29:39 +0500

    add mobile menu

--- js/bundle.js
+++ js/bundle.js
@@ -1,28 +1,43 @@
 window.onload =  () => {
-  heightHeader()
-  preloader()
-}
+  heightHeader();
+  preloader();
+  mmMenu();
+};
 window.onresize = () => {
-  heightHeader()
-  preloader()
-}
+  heightHeader();
+  preloader();
+};
 
 const heightHeader = () => {
-  const navEl = document.querySelector('nav.nav')
-  const navHeight = navEl.offsetHeight
+  const navEl = document.querySelector('nav.nav');
+  const navHeight = navEl.offsetHeight;
   
-  const headerEl = document.querySelector('header.header')
-  const windowHeight = document.body.clientHeight
+  const headerEl = document.querySelector('header.header');
+  const windowHeight = document.body.clientHeight;
 
-  headerEl.style.minHeight = (windowHeight - navHeight) + 'px'
-}
+  headerEl.style.minHeight = (windowHeight - navHeight) + 'px';
+};
 
 const preloader = () => {
-  const preloaderOverlay = document.querySelector('.preloader-overlay')
+  const preloaderOverlay = document.querySelector('.preloader-overlay');
 
   if (!preloaderOverlay.classList.contains('hidden')){
     setTimeout(() => {
-      preloaderOverlay.classList.add('hidden')
+      preloaderOverlay.classList.add('hidden');
     }, 1000)
   }
-}
+};
+
+
+const mmMenu = () => {
+  const menu = mmlight(document.querySelector('.mm-menu'));
+  menu.create();
+  menu.init('selected');
+
+  const mobileMenuButton = document.querySelector('.mobile-menu > li');
+  mobileMenuButton.addEventListener('click', e => {
+    menu.open();
+    e.preventDefault();
+    e.stopPropagation();
+  });
+};
