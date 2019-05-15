7316656568aa0397d6ed5c78f006ead19f6e8b13
Max Chernyshov <chernyshovmaksim56@gmail.com>
2019-05-15 11:23:27 +0500

    5

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ window.onload =  () => {
   heightHeader();
   preloader();
   mmMenu();
+  buttonUp();
+  buttonHidden();
+  initGallery();
+  accordion();
 };
 window.onresize = () => {
   heightHeader();
@@ -24,7 +28,7 @@ const preloader = () => {
   if (!preloaderOverlay.classList.contains('hidden')){
     setTimeout(() => {
       preloaderOverlay.classList.add('hidden');
-    }, 1000)
+    }, 500)
   }
 };
 
@@ -40,4 +44,58 @@ const mmMenu = () => {
     e.preventDefault();
     e.stopPropagation();
   });
+};
+
+const buttonUp = () => {
+  new Waypoint({
+    element: document.querySelector('section.content'),
+    handler: (direction) => {
+      if(direction == 'up'){
+        buttonHidden();
+      }
+      if(direction == 'down'){
+        buttonVisible();
+      }
+    },
+    offset: 20
+  });
+
+  const buttonEl = document.querySelector('.up');
+  buttonEl.addEventListener('click', rollingUp);
+};
+
+const buttonHidden = () => {
+  const buttonEl = document.querySelector('.up');
+  buttonEl.style.transition = 'all 0.17s';
+  buttonEl.style.opacity = 0;
+};
+
+const buttonVisible = () => {
+  const buttonEl = document.querySelector('.up');
+  buttonEl.style.transition = 'all 0.17s';
+  buttonEl.style.opacity = 1;
+};
+
+const rollingUp = () => {
+  const offsetTop = document.documentElement.scrollTop || document.body.scrollTop;
+  if(offsetTop > 0){
+    window.requestAnimationFrame(rollingUp);
+    window.scrollTo(0, offsetTop - offsetTop / 8);
+  }
+};
+
+const initGallery = () => {
+  lightGallery(document.querySelector('.gallery'));
+};
+
+
+const accordion = () => {
+  const accordions = document.querySelectorAll('.accordion');
+  let i;
+  for (i = 0; i < accordions.length; i++){
+    let accHead = accordions[i].querySelector('.accordion-head');
+    accHead.addEventListener('click', () => {
+      accHead.nextElementSibling.classList.toggle('show');
+    });
+  }
 };
